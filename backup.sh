333e52768006f190096350f4710bbe9ffe7a522f
essethon <x.xfzhao@gmail.com>
2020-09-13 11:41:22 +0800

    OS specified

--- backup.sh
+++ backup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-cp ${HOME***REMOVED***/.{vimrc,basic.vimrc,plug.vimrc***REMOVED*** ./
-cp ${HOME***REMOVED***/.gitignore_global ./
-cp ${HOME***REMOVED***/.{zshrc,zsh_functions***REMOVED*** ./
+OS="$(uname -s)"
+case "${OS***REMOVED***" in
+    Darwin*)
+        cp ${HOME***REMOVED***/.{vimrc,basic.vimrc,plug.vimrc***REMOVED*** ./
+        cp ${HOME***REMOVED***/.gitignore_global ./
+        cp ${HOME***REMOVED***/.{zshrc,zsh_functions***REMOVED*** ./
 
-# Remove personal info in git-con***REMOVED***g ***REMOVED***le
-awk '!/name = |email = |signingkey = |essethon/' ${HOME***REMOVED***/.gitcon***REMOVED***g > ./.gitcon***REMOVED***g
+        # Remove personal info in git-con***REMOVED***g ***REMOVED***le
+        awk '!/name = |email = |signingkey = |essethon/' ${HOME***REMOVED***/.gitcon***REMOVED***g > ./.gitcon***REMOVED***g
+        ;;
+    Linux*)
+        cp ${HOME***REMOVED***/.zshrc ./.zshrc-linux-simple
+        ;;
+    *)
+        echo "Unknown OS: ${OS***REMOVED***"
+esac
+
+
