0d4d295b14466aec4b0bd38064a9e8f1886c4f00
essethon <x.xfzhao@gmail.com>
2021-01-23 21:17:46 +0800

    add set linux script

---
+++ setup-linux.sh
@@ -0,0 +1,10 @@
+cp ./.zshrc-linux-simple ~/.zshrc
+cp ./.zsh_functions ~/.zsh_functions
+cp ./.vimrc ~/.vimrc
+cp ./.basic.vimrc ~/.basic.vimrc
+cp ./.plug.vimrc ~/.plug.vimrc
+cp ./.gitcon***REMOVED***g ~/.gitcon***REMOVED***g
+
+echo 'Please set gitcon***REMOVED***g by'
+echo '  git con***REMOVED***g --global user.name <your name>'
+echo '  git con***REMOVED***g --global user.email <your email>'
