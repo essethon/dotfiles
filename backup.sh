ab5a96827ef95e944abb2728f3fb818ec93a53cf
essethon <x.xfzhao@gmail.com>
2021-03-02 21:42:49 +0800

    update zshenv

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ case "${OS***REMOVED***" in
     Darwin*)
         cp ${HOME***REMOVED***/.{vimrc,basic.vimrc,plug.vimrc***REMOVED*** ./
         cp ${HOME***REMOVED***/.gitignore_global ./
-        cp ${HOME***REMOVED***/.{zshrc,zsh_functions***REMOVED*** ./
+        cp ${HOME***REMOVED***/.{zshrc,zshenv,zsh_functions***REMOVED*** ./
         cp ${HOME***REMOVED***/.tmux.conf ./
 
         # Remove personal info in git-con***REMOVED***g ***REMOVED***le
