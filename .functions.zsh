5075c41e4a7952b8d4334f5985a8a2fe542794a7
essethon <x.xfzhao@gmail.com>
2021-03-12 16:36:37 +0800

    rename functions.zsh

--- .functions.zsh
+++ .functions.zsh
@@ -17,6 +17,8 @@ if (( ${+IPINFO_API_TOKEN***REMOVED*** )); then
     curl "ipinfo.io/${1***REMOVED***?token=${IPINFO_API_TOKEN***REMOVED***"
   ***REMOVED***
 ***REMOVED***
+***REMOVED***
+    (( ! ${+commands[drill]***REMOVED*** )) || DNSTOOL=drill
     IP=$(dig +short ${1***REMOVED***)
     echo ${1***REMOVED*** 'resolved to' ${IP***REMOVED***
     curl "ipinfo.io/${IP***REMOVED***?token=${IPINFO_API_TOKEN***REMOVED***"
