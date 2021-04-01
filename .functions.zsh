61d11bd3ab1cf4ccca9fd1451aebf7d5a2cee3f3
essethon <x.xfzhao@gmail.com>
2021-04-01 16:32:49 +0800

    change drill to dig

--- .functions.zsh
+++ .functions.zsh
@@ -18,9 +18,10 @@ if (( ${+IPINFO_API_TOKEN***REMOVED*** )); then
   ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-    (( ! ${+commands[drill]***REMOVED*** )) || DNSTOOL=drill
+    # (( ! ${+commands[drill]***REMOVED*** )) || DNSTOOL=drill
     IP=$($DNSTOOL +short ${1***REMOVED***)
     echo ${1***REMOVED*** 'resolved to' ${IP***REMOVED***
+    IP=$(echo -n $IP | grep -m1 '[0-9]\{1,3\***REMOVED***\.[0-9]\{1,3\***REMOVED***\.[0-9]\{1,3\***REMOVED***\.[0-9]\{1,3\***REMOVED***')
     curl "ipinfo.io/${IP***REMOVED***?token=${IPINFO_API_TOKEN***REMOVED***"
   ***REMOVED***
 ***REMOVED***
